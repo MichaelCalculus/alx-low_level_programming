60b48f33b13d45f09c493bea83188dddd2f88574
MichaelCalculus <Kfrumichael@gmail.com>
2023-10-10 04:13:40 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main A c program that print a line to the standard a e
+rror
+* Return 1 (success)
+*/
+int main(void)
+{
+	fput("and that place of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
+return (0);
+}
+   

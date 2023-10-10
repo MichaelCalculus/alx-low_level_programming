6d56c193014dd06bd068c711a816b0cdb92de8ec
MichaelCalculus <Kfrumichael@gmail.com>
2023-10-10 03:49:20 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various comp
- uter types
+uter types
  * Return 0 (Success)
  */	
 int main(void)
@@ -12,15 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned l
-ong)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned lon
-g)sizeof(b));
-printf("Size of a char: %lu byte(s)\n", (unsigned lo
-ng)sizeof(c));
-printf("Size of a char: %lu byte(s)\n", (unsigned l
-ong)sizeof(d));
-printf("Size of a char: %lu byte(s)\n", (unsigned lon
-g)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
 return(0);
 }

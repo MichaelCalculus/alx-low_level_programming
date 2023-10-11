32c9279dc7211c1a1a97ee1db7a575e5f5264e48
MichaelCalculus <Kfrumichael@gmail.com>
2023-10-11 16:43:25 -0700

    1010-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,9 +5,7 @@
  * Return: 1 (Success)
  */
 int main(void)
-{
-    puts("and that place of art is useful\" - <link>Dora Korp
-ar</link>, 2015-10-19\n", stdout);
-    return (1);
-}
-   
+{	
+	write(2,"and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}  
